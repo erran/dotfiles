a09fec81d490312673160757338b1e26cae99080
Erran Carey <me@errancarey.com>
2014-12-14 18:03:49 -0600

    Add git extensions to ~/.bin

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,12 +7,10 @@ set --export fish_greeting $__fortune
 set --export EDITOR vim
 
 # $PATH
-set --export PATH set PATH $HOME/bin $HOME/.rbenv/bin /usr/local/bin /usr/X11/bin /usr/local/heroku/bin /usr/local/share/npm/bin /Applications/Postgres.app/Contents/Versions/9.3/bin ~/Library/Haskell/bin $PATH
-# set --export GOPATH $HOME/go
-# set --export PATH set PATH $HOME/bin $GOPATH/bin $HOME/.rbenv/bin $HOME/pebble-dev/arm-cs-tools/bin $HOME/bin/packer /usr/local/bin /usr/X11/bin /usr/local/heroku/bin /usr/local/share/npm/bin /Applications/Postgres.app/Contents/MacOS/bin $PATH
+set --export PATH set PATH $HOME/bin $HOME/.bin $HOME/.rbenv/bin /usr/local/bin /usr/X11/bin /usr/local/heroku/bin /usr/local/share/npm/bin /Applications/Postgres.app/Contents/Versions/9.3/bin ~/Library/Haskell/bin $PATH
 
 # Ruby version management
-. (rbenv init -|psub)
+. (rbenv init - | psub)
 set PATH $HOME/.rbenv/bin $PATH
 set PATH $HOME/.rbenv/shims $PATH
 rbenv rehash >/dev/null ^&1
